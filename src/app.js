4562e1020bb68b6a5f1043b07ddb670a3c85944c
hirsch88 <gery.hirschfeld@w3tec.ch>
2016-04-18 14:13:43 +0200

    finishes project

--- src/app.js
+++ src/app.js
@@ -1,54 +1,115 @@
 $(document).ready(function () {
 
-	var map, google, here;
-		
-	// window.navigator.geolocation.getCurrentPosition(showPosition);
-	setEventListener();
-	setView($($('nav > a')[0]).data('view'));
-	
+  // Variables
+  var map, here, service;
+  var formElements = ['Vegi', 'Pizza', 'Burger', 'Sushi'];
+  var selectedFormElement = formElements[1];
+  var iconUrl = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|c68dda';
 
-	// --------------------------------------------------
-	function setSpinner(element) {
-		element.html('<div class="spinner"><i class="fa fa-refresh fa-spin"></i></div>');
-	}
+  // Bootstrapping
+  generateFormElements();
+  setEventListener();
+  setView($($('nav > a')[2]).data('view'));
 
-	function setMap() {
-		console.log('setMap');
-		window.navigator.geolocation.getCurrentPosition(loadMap);
+  // --------------------------------------------------
+  function setEventListener() {
+    // Registers the navigation click event to change the view
+    $('nav > a').on('click', onClickNavigation);
 
-		function loadMap(position) {
-			console.log('loadMap', position);
-			GoogleMapsLoader.load(function(_google) {
-				google = _google;
-				here = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-				map = new google.maps.Map(document.getElementById('map'), {
-					center: here,
-					zoom: 15
-				});
-				console.log('GoogleMapsLoader', google, here, map);
-			});
-		}
-	}
+    // Registers the form action button to search for the given category
+    $('#form').find('.action > button').on('click', onClickFormGo);
+  }
 
-	function setEventListener() {
-		$('nav > a').on('click', onClickNavigation);
-	}
+  function onClickNavigation(event) {
+    event.preventDefault();
+    setView($(event.target).closest('a').data('view'));
+  }
 
-	function onClickNavigation(event) {
-		event.preventDefault();
-		setView($(event.target).closest('a').data('view'));
-	}
+  function onClickFormGo(event) {
+    event.preventDefault();
+    selectedFormElement = $('#form').find('input:checked').val();
+    setView('map');
+  }
 
-	function setView(viewName){
-		if(viewName === 'map'){
-			setSpinner($('#map'));
-			setMap();
-		}
-		$('nav > a').removeClass('active');
-		$('nav > a[data-view="'+viewName+'"]').addClass('active');
+  function setSpinner(element) {
+    element.html('<div class="spinner"><i class="fa fa-refresh fa-spin"></i></div>');
+  }
 
-		$('main > section').hide();
-		$('main > #'+viewName).show();
-	}
+  function setMap() {
+    window.navigator.geolocation.getCurrentPosition(loadMap);
+
+    function loadMap(position) {
+      here = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: here,
+        zoom: 15
+      });
+      setPlaces();
+    }
+  }
+
+  function setPlaces() {
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: here,
+      radius: 500,
+      types: ['restaurant'],
+      keyword: selectedFormElement
+    }, function (results, status) {
+      results.forEach(function (place) {
+        new google.maps.Marker({
+          map: map,
+          position: place.geometry.location,
+          icon: iconUrl
+        });
+      });
+      generatePlaceList(results);
+    });
+  }
+
+  function generatePlaceList(places) {
+    var $list = $('#list').find('ol');
+    $list.empty();
+    var item = function (place, idx) {
+      return '<li id="list-item-' + idx + '">\
+          <b>' + place.name + '</b>\
+          <span>' + place.vicinity + '</span>\
+        </li>';
+    };
+
+    places.forEach(function (place, idx) {
+      $list.append(item(place, idx));
+    });
+  }
+
+  function setView(viewName) {
+    if (viewName === 'map') {
+      setSpinner($('#map'));
+      setMap();
+    }
+    $('nav > a').removeClass('active');
+    $('nav > a[data-view="' + viewName + '"]').addClass('active');
+
+    $('main > section').hide();
+    $('main > #' + viewName).show();
+  }
+
+  function generateFormElements() {
+    var item = function (name, idx) {
+      var checked = (name === selectedFormElement) ? 'checked' : '';
+      return '<div class="form-item">\
+        <input id="form-item-' + idx + '" type="radio" name="from-item" value="' + name + '" ' + checked + '/>\
+        <label for="form-item-' + idx + '">\
+          <i class="fa fa-check"></i>\
+          <span>' + name + '</span>\
+        </label>\
+      </div>';
+    };
+
+    var $formList = $('#form').find('.list');
+    formElements.forEach(function (name, idx) {
+      $formList.append(item(name, idx));
+    });
+  }
 
 });
